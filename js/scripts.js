5733120ebcbbe38c4e2011eee762803ddcbe69c1
matos <matos.gustavo@outlook.com>
2024-08-02 00:46:07 -0300

    implementação da exibição da mensagem informando o vencedor

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ let messageText = document.querySelector("#message p"); // Texto da mensagem de 
 let secondPlayer; // Definição do segundo jogador (IA ou Pessoa)
 let player1 = 0; // Contador de jogadas do jogador 1
 let player2 = 0; // Contador de jogadas do jogador 2
+let winner = 0;
 
 // Validação de quem é a vez da jogada
 function validadorJogada(player1, player2) {
@@ -29,6 +30,7 @@ function computaJogada(jogador1, jogador2) {
 
 // Validação se houve vencedor
 function checkWinCondition(){
+    winner = 0;
     let block1 = document.getElementById("block-1");
     let block2 = document.getElementById("block-2");
     let block3 = document.getElementById("block-3");
@@ -47,9 +49,11 @@ function checkWinCondition(){
         let block3Child = block3.childNodes[0].className;
 
         if (block1Child == "x" && block2Child == "x" && block3Child == "x") {
-            console.log("X venceu");
+            winner = 1;
+            declararWinner("x");
         } else if (block1Child == "o" && block2Child == "o" && block3Child == "o") {
-            console.log("O Venceu");
+            winner = 1;
+            declararWinner("o");
         }
     }
 
@@ -61,9 +65,12 @@ function checkWinCondition(){
         let block6Child = block6.childNodes[0].className;
 
         if (block4Child == "x" && block5Child == "x" && block6Child == "x") {
-            console.log("X venceu");
+            winner = 1;
+            declararWinner("x");
         } else if (block4Child == "o" && block5Child == "o" && block6Child == "o") {
-            console.log("O Venceu");
+            winner = 1;
+            winner = 1;
+            declararWinner("o");
         }
     }
 
@@ -75,9 +82,11 @@ function checkWinCondition(){
         let block9Child = block9.childNodes[0].className;
 
         if (block7Child == "x" && block8Child == "x" && block9Child == "x") {
-            console.log("X venceu");
+            winner = 1;
+            declararWinner("x");
         } else if (block7Child == "o" && block8Child == "o" && block9Child == "o") {
-            console.log("O Venceu");
+            winner = 1;
+            declararWinner("o");
         }
     }
 
@@ -89,9 +98,11 @@ function checkWinCondition(){
         let block7Child = block7.childNodes[0].className;
 
         if (block1Child == "x" && block4Child == "x" && block7Child == "x") {
-            console.log("X venceu");
+            winner = 1;
+            declararWinner("x");
         } else if (block1Child == "o" && block4Child == "o" && block7Child == "o") {
-            console.log("O Venceu");
+            winner = 1;
+            declararWinner("o");
         }
     }
 
@@ -103,9 +114,11 @@ function checkWinCondition(){
         let block8Child = block8.childNodes[0].className;
 
         if (block2Child == "x" && block5Child == "x" && block8Child == "x") {
-            console.log("X venceu");
+            winner = 1;
+            declararWinner("x");
         } else if (block2Child == "o" && block5Child == "o" && block8Child == "o") {
-            console.log("O Venceu");
+            winner = 1;
+            declararWinner("o");
         }
     }
 
@@ -117,9 +130,11 @@ function checkWinCondition(){
         let block9Child = block9.childNodes[0].className;
 
         if (block3Child == "x" && block6Child == "x" && block9Child == "x") {
-            console.log("X venceu");
+            winner = 1;
+            declararWinner("x");
         } else if (block3Child == "o" && block6Child == "o" && block9Child == "o") {
-            console.log("O Venceu");
+            winner = 1;
+            declararWinner("o");
         }
     }
 
@@ -131,9 +146,11 @@ function checkWinCondition(){
         let block9Child = block9.childNodes[0].className;
 
         if (block1Child == "x" && block5Child == "x" && block9Child == "x") {
-            console.log("X venceu");
+            winner = 1;
+            declararWinner("x");
         } else if (block1Child == "o" && block5Child == "o" && block9Child == "o") {
-            console.log("O Venceu");
+            winner = 1;
+            declararWinner("o");
         }
     }
     
@@ -145,9 +162,11 @@ function checkWinCondition(){
         let block7Child = block7.childNodes[0].className;
 
         if (block3Child == "x" && block5Child == "x" && block7Child == "x") {
-            console.log("X venceu");
+            winner = 1;
+            declararWinner("x");
         } else if (block3Child == "o" && block5Child == "o" && block7Child == "o") {
-            console.log("O Venceu");
+            winner = 1;
+            declararWinner("o");
         }
     }
 
@@ -160,14 +179,57 @@ function checkWinCondition(){
         }
     }
 
-    if (counterJogadas == 9) {
-        console.log("Deu velha");
+    if (counterJogadas == 9 && winner == 0) {
+        declararWinner("velha");
     }
 }
 
+// Declaração Vencedor, exibição da mensagem de vencedor
+function declararWinner(winner) {
+
+    let scoreBoardX = document.querySelector("#scoreboard-1");
+    let scoreBoardO = document.querySelector("#scoreboard-2");
+    let messageWinner = "";
+
+    if (winner == "x") {
+        messageWinner = "Jogador 1 venceu!";
+        scoreBoardX.textContent = parseInt(scoreBoardX.textContent) + 1;
+        
+    } else if (winner == "o") {
+        messageWinner = "Jogador 2 venceu!";
+        scoreBoardO.textContent = parseInt(scoreBoardO.textContent) + 1;
+        
+    } else {
+        messageWinner = "Deu velha!";
+    }
+
+    messageText.innerHTML = messageWinner;
+    messageContainer.classList.remove("hide");
+
+    setTimeout(function() {
+        messageContainer.classList.add("hide");
+    }, 2500);
+
+    player1 = 0;
+    player2 = 0;
+
+    removeJogadas()
+}
+
+// Limpa tela
+function removeJogadas(){
+    let boxesToRemove = document.querySelectorAll(".box div");
+
+    setTimeout(function() {
+        for (let index = 0; index < boxesToRemove.length; index++) {
+            boxesToRemove[index].parentNode.removeChild(boxesToRemove[index]);
+        }
+    }, 2500);
+}
+
+
 // Adicionando o evento de click dentro das boxes
 for (let index = 0; index < boxes.length; index++) {
-
     // Quando alguem clicar na caixa
     boxes[index].addEventListener("click", function () {
 
@@ -181,6 +243,7 @@ for (let index = 0; index < boxes.length; index++) {
             computaJogada(player1, player2);
 
             checkWinCondition();
+
         }
     });
 }
